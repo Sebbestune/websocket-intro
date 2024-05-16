2b4b79dac16dfcff0ce36c009f8c64f5cf57c6a2
Sebastian Lindgren <sebastian.tb.lindgren@gmail.com>
2024-05-16 10:21:00 +0200

    Chat design and send html

--- chat/index.js
+++ chat/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000, () => {
-  console.log('listening on http://localhost:3000');
+    console.log('listening on http://localhost:3000');
 });
