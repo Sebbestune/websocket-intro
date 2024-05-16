82e86d56af7fe6e66d0cb7b14b2580f89abb5e21
Sebastian Lindgren <sebastian.tb.lindgren@gmail.com>
2024-05-16 15:46:27 +0200

    Design and username

--- chat/index.ts
+++ chat/index.ts
@@ -10,31 +10,47 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  (socket as any).username = socket.id;
+  console.log("A user connected: named: " + (socket as any).username);
 
   socket.join("room1");
 
   socket.on("change room", (room) => {
-    io.emit(
-      "chat message",
-      "User: " +
-        socket.id +
-        " changed room to: " +
-        room
-    );
+    io.emit("chat message", "User: " + (socket as any).username + " changed room to: " + room);
 
     socket.leave(Array.from(socket.rooms)[1]);
     socket.join(room);
     const printableArray = Array.from(socket.rooms);
 
-    console.log(socket.id + " now in rooms " + printableArray + " total " + printableArray.length);
+    console.log(
+      (socket as any).username +
+        " now in rooms " +
+        printableArray +
+        " total " +
+        printableArray.length
+    );
   });
 
   socket.on("chat message", (msg) => {
-    io.to(Array.from(socket.rooms)[1]).emit("chat message", msg);
+    io.to(Array.from(socket.rooms)[1]).emit(
+      "chat message",
+      (socket as any).username + " said: " + msg
+    );
   });
+
+  socket.on("change username", (username) => {
+    const prevName = (socket as any).username;
+    console.log("Prev name: " + prevName);
+    (socket as any).username = username;
+    console.log("New name: " + (socket as any).username);
+    io.to(Array.from(socket.rooms)[1]).emit(
+      "chat message",
+      prevName + "changed name to " + (socket as any).username
+    );
+  });
+
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    console.log((socket as any).username + " disconnected");
   });
 });
 
